45dee34bd4098fb225146db58c6151bac0191547
Rudinilly-ti <rudinilly-ti@outolook.com>
2023-04-13 22:36:51 -0300

    Questao 5 att

--- dynamicProgamming/Questao5.js
+++ dynamicProgamming/Questao5.js
@@ -6,7 +6,7 @@ function findMaxTriangleHeight(columnHeights) {
       // Calcula a altura do triângulo formado ao remover os cubos de cima para baixo em colunas
       // A altura do triângulo será a altura mínima entre as colunas subtraída do índice (i)
       const minIndex = columnHeights.indexOf(Math.min(...columnHeights));
-      const triangleHeight = Math.min(...columnHeights) - i + 1; // corrigido: adiciona +1 ao resultado
+      const triangleHeight = Math.min(...columnHeights) - i;
 
       // Atualiza a altura máxima se a altura do triângulo atual for maior
       if (triangleHeight > maxHeight) {
@@ -21,7 +21,7 @@ function findMaxTriangleHeight(columnHeights) {
 }
 
 // Leitura de entrada
-const input = "5 6 5 8 9 10 5 8 9 5 7 9 9 9 6 3";
+const input = "5 1 1 1 1 1 1 3";
 const inputArray = input.split(" ");
 const n = parseInt(inputArray[0]); // número de colunas na parede
 const columnHeights = inputArray.slice(1).map(height => parseInt(height)); // alturas de cada coluna como um array
