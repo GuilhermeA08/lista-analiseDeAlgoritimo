8522aa02b6869b178c08f555552eec6824567121
Rudinilly-ti <rudinilly-ti@outolook.com>
2023-04-13 22:35:18 -0300

    Questao 5 att

--- dynamicProgamming/Questao5.js
+++ dynamicProgamming/Questao5.js
@@ -1,21 +1,31 @@
 function findMaxTriangleHeight(columnHeights) {
-  const n = columnHeights.length; 
-  let maxHeight = 0;
+  const n = columnHeights.length; // número de colunas
+  let maxHeight = 0; // inicializa altura máxima como 0
 
   for (let i = 0; i < n; i++) {
-      const triangleHeight = Math.min(...columnHeights) - i;
+      // Calcula a altura do triângulo formado ao remover os cubos de cima para baixo em colunas
+      // A altura do triângulo será a altura mínima entre as colunas subtraída do índice (i)
+      const minIndex = columnHeights.indexOf(Math.min(...columnHeights));
+      const triangleHeight = Math.min(...columnHeights) - i + 1; // corrigido: adiciona +1 ao resultado
+
+      // Atualiza a altura máxima se a altura do triângulo atual for maior
       if (triangleHeight > maxHeight) {
           maxHeight = triangleHeight;
       }
-      const minIndex = columnHeights.indexOf(Math.min(...columnHeights));
+
+      // Remove o cubo do topo da coluna com altura mínima
       columnHeights[minIndex] -= 1;
   }
 
   return maxHeight;
 }
 
- var columnHeights = "10 5 20 10 5 7 7 5".split(" ")
-  .map(height => parseInt(height));
-  
+// Leitura de entrada
+const input = "5 6 5 8 9 10 5 8 9 5 7 9 9 9 6 3";
+const inputArray = input.split(" ");
+const n = parseInt(inputArray[0]); // número de colunas na parede
+const columnHeights = inputArray.slice(1).map(height => parseInt(height)); // alturas de cada coluna como um array
+
+// Chama a função e imprime o resultado
 const maxTriangleHeight = findMaxTriangleHeight(columnHeights);
 console.log(maxTriangleHeight);
